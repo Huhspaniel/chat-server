139224372ed43f7576e663ed2655687f5b2e240a
Huhspaniel <41580234+Huhspaniel@users.noreply.github.com>
2019-01-31 12:19:33 -0500

    Add reconnect button to client page
    Add messages to notify user of relevant information
    about the connection

--- app/client/index.js
+++ app/client/index.js
@@ -1,102 +1,132 @@
-const isSec = window.location.protocol === 'https:'
-const socket = new WebSocket(`${isSec ? 'wss' : 'ws'}://${window.location.hostname}:${window.location.port}`);
-
 const messages = document.querySelector('.messages');
-const inputBox = document.querySelector('.user-input input');
 const form = document.querySelector('.user-input');
-
-socket.addEventListener('error', err => {
-    console.log(err);
-})
-
+const reconnectBtn = document.querySelector('button.reconnect');
 let loggedIn = false;
 let myUsername = null;
+function renderMessage(tag, msg, msgClass, tagClass) {
+    messages.innerHTML =
+        `<div class="${msgClass || ''} message">
+                <span class="${tagClass || ''}">${tag}:</span> ${msg}
+        </div>` + messages.innerHTML;
+}
 
-socket.addEventListener('message', event => {
-    console.log('Received: ', event.data);
-    const data = JSON.parse(event.data);
-    switch (data.event) {
-        case 'chat': {
-            const [username, chat] = data.args;
-            let scrollTop = messages.scrollTop;
-            messages.innerHTML =
-                `<div class="chat message">
-                <span class="${username === myUsername ? 'me' : 'user'}">@${username}:</span> ${chat}
-            </div>` + messages.innerHTML;
-            if (messages.scrollHeight - messages.scrollTop > messages.offsetHeight) {
-                messages.scrollTop = scrollTop;
-            }
+const isSec = window.location.protocol === 'https:'
+const socketUrl = `${isSec ? 'wss' : 'ws'}://${window.location.hostname}:${window.location.port}`;
+let socket = { readyState: 3 };
+function connectSocket() {
+    console.log(socket);
+    const { readyState } = socket;
+    switch (readyState) {
+        case 2: {
             break;
         }
-        case 'login': {
-            const [username] = data.args;
-            messages.innerHTML =
-                `<div class="login message${username === myUsername ? ' me' : ''}">
-                <span>@${username}</span> has joined the chatroom
-            </div>` + messages.innerHTML;
-            if (!loggedIn && username === myUsername) {
-                loggedIn = true;
-                inputBox.setAttribute('placeholder', '');
-            }
+        case 3: {
             break;
         }
-        case 'logout': {
-            const [username] = data.args;
-            messages.innerHTML =
-                `<div class="logout message">
-                <span class=${username === myUsername ? 'me' : ''}>@${username}</span> has left the chatroom
-            </div>` + messages.innerHTML;
-            break;
-        }
-        case 'error': {
-            const [err] = data.args;
-            messages.innerHTML =
-                `<div class="error message">
-                <span>ERROR:</span> ${err}
-            </div>` + messages.innerHTML;
-            break;
-        }
-        case 'info': {
-            const [info] = data.args;
-            messages.innerHTML =
-                `<div class="info message">
-                <span>SERVER:</span> ${info}
-            </div>` + messages.innerHTML;
-            break;
+        default: {
+            socket.close();
         }
     }
-});
+    socket = new WebSocket(socketUrl);
+    socket.onopen = event => {
+        console.log('Connection to server established: ', event);
+        renderMessage('CLIENT', 'Connection to server established', 'client');
+    };
+    socket.onclose = event => {
+        loggedIn = false;
+        form[0].placeholder = 'Input a username';
+        console.log('Disconnected from server: ', event);
+        renderMessage('CLIENT', 'Disconnected from server', 'client')
+    }
+    socket.onerror = event => {
+        console.log('WebSocket error event: ', event);
+        renderMessage('ERROR', 'Caught WebSocket error (in console)', 'error');
+    };
+    socket.onmessage = event => {
+        console.log('Received:', JSON.parse(event.data));
+        const data = JSON.parse(event.data);
+        switch (data.event) {
+            case 'chat': {
+                const [username, chat] = data.args;
+                let scrollTop = messages.scrollTop;
+                messages.innerHTML =
+                    `<div class="chat message">
+                    <span class="${username === myUsername ? 'me' : 'user'}">@${username}:</span> ${chat}
+                </div>` + messages.innerHTML;
+                if (messages.scrollHeight - messages.scrollTop > messages.offsetHeight) {
+                    messages.scrollTop = scrollTop;
+                }
+                break;
+            }
+            case 'login': {
+                const [username] = data.args;
+                messages.innerHTML =
+                    `<div class="login message${username === myUsername ? ' me' : ''}">
+                    <span>@${username}</span> has joined the chatroom
+                </div>` + messages.innerHTML;
+                if (!loggedIn && username === myUsername) {
+                    loggedIn = true;
+                    form[0].placeholder = '';
+                }
+                break;
+            }
+            case 'logout': {
+                const [username] = data.args;
+                messages.innerHTML =
+                    `<div class="logout message">
+                    <span class=${username === myUsername ? 'me' : ''}>@${username}</span> has left the chatroom
+                </div>` + messages.innerHTML;
+                break;
+            }
+            case 'error': {
+                const [err] = data.args;
+                messages.innerHTML =
+                    `<div class="error message">
+                    <span>ERROR:</span> ${err}
+                </div>` + messages.innerHTML;
+                break;
+            }
+            case 'info': {
+                const [info] = data.args;
+                messages.innerHTML =
+                    `<div class="server message">
+                    <span>SERVER:</span> ${info}
+                </div>` + messages.innerHTML;
+                break;
+            }
+        }
+    };
+}
+connectSocket();
+reconnectBtn.addEventListener('click', connectSocket);
 
-// inputBox.addEventListener('keyup', e => {
-//     if (e.key === 'Enter') {
-//         const input = e.target.value;
-//         e.target.value = '';
-//         const message = {
-//             event: null,
-//             args: [input]
-//         }
-//         if (loggedIn) {
-//             message.event = 'chat';
-//         } else {
-//             message.event = 'login';
-//             myUsername = input;
-//         }
-//         socket.send(JSON.stringify(message));
-//     }
-// });
 form.addEventListener('submit', e => {
     e.preventDefault();
-    const input = e.target[0].value;
-    e.target[0].value = '';
-    const message = {
-        event: null,
-        args: [input]
+    const { readyState } = socket;
+    switch (readyState) {
+        case 0: {
+            renderMessage('INFO', 'Connecting to server... please wait', 'info');
+            break;
+        }
+        case 1: {
+            const input = e.target[0].value;
+            e.target[0].value = '';
+            const message = {
+                event: null,
+                args: [input]
+            }
+            if (loggedIn) {
+                message.event = 'chat';
+            } else {
+                message.event = 'login';
+                myUsername = input;
+            }
+            socket.send(JSON.stringify(message));
+            break;
+        }
+        default: {
+            renderMessage('ERROR', 'Not connected to server', 'error');
+            break;
+        }
     }
-    if (loggedIn) {
-        message.event = 'chat';
-    } else {
-        message.event = 'login';
-        myUsername = input;
-    }
-    socket.send(JSON.stringify(message));
 })
